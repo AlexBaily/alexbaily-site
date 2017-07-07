df3852b34d3e38af6d963a097eaa0222e962457d
AlexBaily <alex.d.baily@gmail.com>
2017-07-07 07:15:25 +0000

    fix typos and remove erroneous code

--- app.js
+++ app.js
@@ -9,19 +9,6 @@ var instances = ['server1', 'server2', 'server3']
 var AWS = require("aws-sdk");
 AWS.config.update({region:'eu-west-1'});
 
-
-var getMessages = function () {
- var docClient = new AWS.DynamoDB.DocumentClient();
- var params = { TableName: "message-table", KeyConditionExpression: "chatID = :chatID", ExpressionAttributeValues: { ":chatID":"1" }};
-
-  var messages = ""
-  docClient.query(params, function(err, data) {
-    if (err) { console.log(err); }
-    else { console.log(data.Items[0].message); var messages = data; }
-  });
-};
-var test = getMessages();
-
 //Set the view engine to jade
 app.set('view engine', 'jade');
 app.use(express.static('public'));
@@ -29,29 +16,32 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 //Returning responses for GET requires
 app.get('/', function (req, res) {
-  var test = getMessages();
+
   var docClient = new AWS.DynamoDB.DocumentClient();
   var params = { TableName: "message-table", KeyConditionExpression: "chatID = :chatID", ExpressionAttributeValues: { ":chatID":"1" }};
-
-  var messages = ""
+  
+  //Query the dynamoDB table to get the information on the messages
+  //Todo: seperate getting the DynamoDB info into an API query and offload fetching of the data to the client side
   docClient.query(params, function(err, data) {
     if (err) { console.log(err); }
     else { 
-      console.log(data.Items[0].message); 
-      var messages = data; 
-   res.render('index',
+     console.log(data.Items[0].message); 
+     res.render('index',
               { title: 'Hello world!',
                 message: 'Welcome to the world of tomorrow!',
                 platform: osVer,
                 servers: instances,
-                        messageList: data.Items[0].message});
-  }})});
+                messageList: data.Items[0].message});
+    }
+  })
+});
 
-
+//Monitor URL for the load balancer
 app.get('/monitor.htm', function (req, res) {
   res.sendFile(__dirname + '/monitor.htm');
 });
 
+//Currently does nothing
 app.post('/', function(req, res) {
     var message = req.body.message;
     if (message) {
