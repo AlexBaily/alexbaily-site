324030bdf3f9c328fb7c63de8a3ce2c36c5b6a4c
AlexBaily <alex.d.baily@gmail.com>
2017-07-07 07:04:54 +0000

    add css and dynamoDB access to support messaging
    Added additional pages for css to help test. Added the AWS SDK and added reading from a DynamoDB table that will allow us to read from messages.

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
 const express = require('express');
 const app = express();
 var os = require('os');
+var bodyParser = require('body-parser');
 
 var osVer = os.platform();
+var instances = ['server1', 'server2', 'server3']
+
+var AWS = require("aws-sdk");
+AWS.config.update({region:'eu-west-1'});
+
+
+var getMessages = function () {
+ var docClient = new AWS.DynamoDB.DocumentClient();
+ var params = { TableName: "message-table", KeyConditionExpression: "chatID = :chatID", ExpressionAttributeValues: { ":chatID":"1" }};
+
+  var messages = ""
+  docClient.query(params, function(err, data) {
+    if (err) { console.log(err); }
+    else { console.log(data.Items[0].message); var messages = data; }
+  });
+};
+var test = getMessages();
 
 //Set the view engine to jade
 app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //Returning responses for GET requires
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hello world!', message: 'Welcome to the world of tomorrow!', platform: osVer });
-});
+  var test = getMessages();
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var params = { TableName: "message-table", KeyConditionExpression: "chatID = :chatID", ExpressionAttributeValues: { ":chatID":"1" }};
+
+  var messages = ""
+  docClient.query(params, function(err, data) {
+    if (err) { console.log(err); }
+    else { 
+      console.log(data.Items[0].message); 
+      var messages = data; 
+   res.render('index',
+              { title: 'Hello world!',
+                message: 'Welcome to the world of tomorrow!',
+                platform: osVer,
+                servers: instances,
+                        messageList: data.Items[0].message});
+  }})});
+
 
 app.get('/monitor.htm', function (req, res) {
   res.sendFile(__dirname + '/monitor.htm');
+});
+
+app.post('/', function(req, res) {
+    var message = req.body.message;
+    if (message) {
+       console.log(message);
+       res.redirect("/");
+    }
 });
 
 app.listen(8080, function () {
