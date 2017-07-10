1cdb64b2f7ce378af5d307478bf7311597704568
AlexBaily <alex.d.baily@gmail.com>
2017-07-10 18:24:45 +0000

    add support for a list of messages

--- app.js
+++ app.js
@@ -6,80 +6,97 @@ var bodyParser = require('body-parser');
 var osVer = os.platform();
 var instances = ['server1', 'server2', 'server3']
 var AWS = require("aws-sdk");
-AWS.config.update({region:'eu-west-1'});
+AWS.config.update({
+    region: 'eu-west-1'
+});
 
 //Set the view engine to jade
 app.set('view engine', 'jade');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 //Returning responses for GET requires
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 
-  //Query the dynamoDB table to get the information on the messages
-  //Todo: seperate getting the DynamoDB info into an API query and offload fetching of the data to the client side
-     res.render('index',
-              { title: 'Hello world!',
-                message: 'Welcome to the world of tomorrow!',
-                platform: osVer,
-                servers: instances});
+    //Render the base page using Jade, other information will be gathered via API
+    res.render('index', {
+        title: 'Hello world!',
+        message: 'Welcome to the world of tomorrow!',
+        platform: osVer,
+        servers: instances
+    });
 });
 
 //Monitor URL for the load balancer
-app.get('/monitor.htm', function (req, res) {
-  res.sendFile(__dirname + '/monitor.htm');
-});
-
-app.get('/api/messages', function (req, res) {
-	
-  var docClient = new AWS.DynamoDB.DocumentClient();
-  var params = { TableName: "message-table", 
-                             KeyConditionExpression: "chatID = :chatID", 
-                             ExpressionAttributeValues: { ":chatID":"1" }};	
-  
-  docClient.query(params, function(err, data) {
-	if (err) { console.log(err) }  
-	else {
-                var messageList = {
-                     chatID: data.Items[0].chatID,
-                     message: data.Items[0].message
-                };
-		res.send (messageList);
-	}
-  });
-	
+app.get('/monitor.htm', function(req, res) {
+    res.sendFile(__dirname + '/monitor.htm');
 });
 
 
-//Currently does nothing
+//pseudo "API" for gathering DynamoDB information, still required is client authentication
+app.get('/api/messages', function(req, res) {
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "message-table",
+        KeyConditionExpression: "chatID = :chatID",
+        ExpressionAttributeValues: {
+            ":chatID": "1"
+        }
+    };
+
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            var messageList = {
+                chatID: data.Items[0].chatID,
+                message: data.Items[0].message
+            };
+            res.send(messageList);
+        }
+    });
+});
+
+
+//Post requests will amend the chat to DynamoDB.
+//TODO: Sanitize inputs, create OPTIONS, require auth?
 app.post('/', function(req, res) {
     var message = req.body.message;
     if (message) {
-       console.log(message);
-       var docClient = new AWS.DynamoDB.DocumentClient();
-       var params = {
+        console.log(message);
+        var docClient = new AWS.DynamoDB.DocumentClient();
+        var params = {
             TableName: "message-table",
-            Item:{
-                "chatID": "1",
-                "message": message
+            Key: {
+                "chatID": "1"
+            },
+            UpdateExpression: "SET #attrName = list_append(#attrName, :attrValue)",
+            ExpressionAttributeNames: {
+                "#attrName": "message"
+            },
+            ExpressionAttributeValues: {
+                ":attrValue": [message]
             }
-       };
-       docClient.put(params, function(err, data) {
-          if (err) {
-             console.error(err);
-          }
-          else { 
-             res.redirect("/");
-          }
-       });
+        };
+        docClient.update(params, function(err, data) {
+            if (err) {
+                console.error(err);
+            } else {
+                res.redirect("/");
+            }
+        });
     }
 });
 
-app.listen(8080, function () {
-  console.log('The application is listening on port 8080')
+//Starts the application on port 8080
+app.listen(8080, function() {
+    console.log('The application is listening on port 8080')
 });
 
-app.use(function (req, res) {
-        res.status(404).send('ERROR MATE! WRONG PAGE!:');
+//404 page
+app.use(function(req, res) {
+    res.status(404).send('ERROR MATE! WRONG PAGE!:');
 });
-
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
-function getMessages() {
-  document.getElementById("messageList").innerHTML = "test";
-}
-
-
 $(document).ready(function(){
   $.get("/api/messages", function(data, status) {
-    document.getElementById("messageList").innerHTML = data.message;
+    var list = document.getElementById("messageList");
+    var messages = data.message;
+
+    messages.forEach(function(message) {
+      var entry = document.createElement('li');
+      entry.appendChild(document.createTextNode(message));
+      list.appendChild(entry);
+    });
     console.log(data);
   });
 });
