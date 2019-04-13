9d561c73c58988b0307d4376b22f8770ad200d3a
AlexBaily <alex.d.baily@gmail.com>
2019-04-13 18:58:22 +0000

    Add JS to interact with the register API to allow the form to submit data.

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,43 @@
 $(document).ready(function(){
-  $.get("/api/messages", function(data, status) {
-    var list = document.getElementById("messageList");
-    var messages = data.message;
+    $.get("/api/messages", function(data, status) {
+        var list = document.getElementById("messageList");
+        var messages = data.message;
 
-    messages.forEach(function(message) {
-      var entry = document.createElement('li');
-      entry.appendChild(document.createTextNode(message));
-      list.appendChild(entry);
+        messages.forEach(function(message) {
+          var entry = document.createElement('li');
+          entry.appendChild(document.createTextNode(message));
+          list.appendChild(entry);
+        });
+        console.log(data);
     });
-    console.log(data);
-  });
+
+    var registerForm = document.getElementById('register-form');
+
+    registerForm.onsubmit = function(event) {
+        //Stops the form from send the data before we intercept.
+        event.preventDefault();
+
+        var fd = new FormData(registerForm);
+        const data = new URLSearchParams(fd);
+        for (var [key, value] of fd.entries()) { 
+         console.log(key, value);
+         }
+        fetch("/api/register", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: data
+        }).then(function(res) {
+        if (res.ok) {
+            alert("Thanks! You are almost there, " + 
+            "please follow the verification link sent via email to " +
+            "finish registration.");
+        } else if (res.status == 401) {
+            alert("Oops! You are not authorized.");
+        }
+        }, function(e) {
+            alert("Error submitting form!");
+        });
+    }    
 });
